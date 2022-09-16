479b78e5d3097ee23b3b818014ff96b71fc1e35f
bumman <anhbumbk@gmail.com>
2022-09-16 13:57:00 +0700

    buoi 9

--- buoi4.baitap/src/main/java/com/example/vmg/Controller/BlogRestController.java
+++ buoi4.baitap/src/main/java/com/example/vmg/Controller/BlogRestController.java
@@ -2,12 +2,26 @@ package com.example.vmg.Controller;
 
 import com.example.vmg.exception.NoDataFoundException;
 import com.example.vmg.model.Blog;
+import com.example.vmg.model.BlogForm;
+import com.example.vmg.model.Category;
+import com.example.vmg.model.Cover;
 import com.example.vmg.service.BlogService;
+import com.example.vmg.service.CategoryService;
+import com.example.vmg.service.CoverService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +32,27 @@ public class BlogRestController {
 
     @Autowired
     private BlogService blogService;
+
+    @Autowired
+    private CoverService coverService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Value("${upload.path}")
+    private String fileUpload;
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> getCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        if (categories.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
 
     @GetMapping("/blogs")
     public ResponseEntity<List<Blog>> getBlogList(@RequestParam(value = "title", required = false) String title,
@@ -57,10 +92,40 @@ public class BlogRestController {
         return new ResponseEntity<>(blogService.getBlog(id), HttpStatus.OK);
     }
 
-//    @PostMapping("/blogs")
-//    public ResponseEntity<Blog> createBlog(@RequestBody Blog blog) {
-//
-//    }
+    @PostMapping("/blogs")
+    public ResponseEntity<Blog> createBlog(@ModelAttribute BlogForm blogForm,
+                                           BindingResult result) {
+        System.out.println("add blog running...");
+        System.out.println(blogForm.getCategory());
+        Category category = categoryService.getCategory(blogForm.getCategory().getId());
+        System.out.println(category.getName());
+        blogForm.setCategory(category);
+        System.out.println(blogForm.getTitle());
+        if (result.hasErrors()) {
+            for (Object object : result.getAllErrors()) {
+                if(object instanceof FieldError) {
+                    FieldError fieldError = (FieldError) object;
+
+                    /**
+                     * Use null as second parameter if you do not use i18n (internationalization)
+                     */
+
+                    String message = messageSource.getMessage(fieldError, null);
+                    System.out.println(message);
+                }
+            }
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Blog blog = new Blog.BlogBuilder(blogForm.getTitle())
+                .content(blogForm.getContent())
+                .build();
+//        Category category1 = blog.getCategory();
+        blog.setCategory(category);
+        blogService.add(blog);
+        addFile(blogForm.getFiles(), blog);
+
+        return new ResponseEntity<>(blog, HttpStatus.CREATED);
+    }
 
     @DeleteMapping("/blogs/{id}")
     public ResponseEntity<HttpStatus> deleteBlog(@PathVariable("id") long id) {
@@ -69,6 +134,20 @@ public class BlogRestController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void addFile(List<MultipartFile> files, Blog blog) {
+        for (MultipartFile file : files) {
+            try {
+                var fileName = file.getOriginalFilename();
+                var is = file.getInputStream();
+                Files.copy(is, Paths.get(this.fileUpload + fileName), StandardCopyOption.REPLACE_EXISTING);
+                Cover cover = new Cover(fileName, blog);
+                coverService.save(cover);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
